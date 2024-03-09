f491d1882b2e103ef01495a99b06609688839ed1
furkanucgull <furkanucgull@gmail.com>
2024-03-09 16:10:49 +0100

    NewTablemigratedtoBannerEntity

--- CarBook/Infrastructure/CarBook.Persistance/Migrations/20240309150607_AddNewTable.Designer.cs
+++ CarBook/Infrastructure/CarBook.Persistance/Migrations/20240309150607_AddNewTable.Designer.cs
@@ -4,6 +4,7 @@ using CarBook.Persistance.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarBook.Persistance.Migrations
 {
     [DbContext(typeof(CarBookContext))]
-    partial class CarBookContextModelSnapshot : ModelSnapshot
+    [Migration("20240309150607_AddNewTable")]
+    partial class AddNewTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarBook/Infrastructure/CarBook.Persistance/Migrations/20240309150607_AddNewTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarBook.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VideoDescription",
+                table: "Banners",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VideoDescription",
+                table: "Banners");
+        }
+    }
+}
