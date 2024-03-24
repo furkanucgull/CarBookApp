6c359e5c428024ddf31d7e88cf1ef9a1e8023791
furkanucgull <furkanucgull@gmail.com>
2024-03-24 17:44:26 +0100

    MvcController_ViewComponents

---
+++ CarBook/Frontends/CarBook.Web.UI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.Web.UI.ViewComponents.UILayoutViewComponents
+{
+    public class _NavbarUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
